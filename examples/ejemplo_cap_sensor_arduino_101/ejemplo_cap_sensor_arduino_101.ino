824db570337eb0e89e1b0a6070684f4d40d67109
Jorge Gomez <syvic@sindormir.net>
2018-12-09 09:50:18 +0100

    Añadido ejemplo de uso sencillo para la 101

--- examples/ejemplo_cap_sensor_arduino_101/ejemplo_cap_sensor_arduino_101.ino
+++ examples/ejemplo_cap_sensor_arduino_101/ejemplo_cap_sensor_arduino_101.ino
@@ -1,31 +1,21 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor   cs_4_2 = CapacitiveSensor(2,3);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
-//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+CapacitiveSensor cs_2_3 = CapacitiveSensor(2,3); // 10M resistor between pins 2 & 3, pin 3 is sensor pin, add a wire and or foil if desired
 
-void setup()                    
-{
-   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+void setup() {
+   cs_2_3.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
    Serial.println("Arrancando");
 }
 
-void loop()                    
-{
-    long start = millis();
-    long total1 =  cs_4_2.capacitiveSensor(30);
-    //long total2 =  cs_4_6.capacitiveSensor(30);
-    //long total3 =  cs_4_8.capacitiveSensor(30);
+void loop() {
+    unsigned long start = millis();
+    long total =  cs_2_3.capacitiveSensor(30);
 
     Serial.print(millis() - start);        // check on performance in milliseconds
     Serial.print("\t");                    // tab character for debug windown spacing
 
-    Serial.print(total1);                  // print sensor output 1
-    Serial.println("\t");
-    //Serial.print(total2);                  // print sensor output 2
-    //Serial.print("\t");
-    //Serial.println(total3);                // print sensor output 3
+    Serial.println(total);                 // print sensor output 
 
     delay(10);                             // arbitrary delay to limit data to serial port 
 }
